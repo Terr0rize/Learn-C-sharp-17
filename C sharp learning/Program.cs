b039b04bc2ddff3047230183e2ba7e77e16b7cce
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-09-03 16:54:55 +0400

    Add files via upload

---
+++ C sharp learning/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic; //массив, списки.
+using System.ComponentModel;
+using System.Globalization;
+using System.Net;
+
+namespace C_sharp_learning
+{
+    
+    enum Breed {Dog, Cat, Bird, Bead, Tiger };
+
+
+   class Animal
+    {
+        public Breed breed;
+
+      public  Animal(Breed breed)
+        {
+            this.breed = breed;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(breed);
+        }
+    }
+
+
+    class MainClass
+    {   
+        public static void Main(string[] args)
+        {
+            Animal dog = new Animal(Breed.Bird);
+            dog.Print();
+            Console.ReadLine();
+        }
+
+    }
+}
